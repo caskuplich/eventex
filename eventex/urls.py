3cbb497a19ac99838b80f6eb9f9e030493cc392a
Cassiano Kuplich <crkuplich@openmailbox.org>
2016-09-03 18:30:32 -0300

    Update urls to 1.10.1

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from eventex.core import views
 
 urlpatterns = [
-    url(r'^$', 'eventex.core.views.home'),
+    url(r'^$', views.home),
     url(r'^admin/', include(admin.site.urls)),
 ]
