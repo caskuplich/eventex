2299f6c764488a0998e7ad2135b6445c11b98cc5
Cassiano Kuplich <crkuplich@openmailbox.org>
2016-09-26 18:34:33 -0300

    Email or phone are optional

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -4,7 +4,7 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 class SubscriptionFormTest(TestCase):
     def test_form_has_fields(self):
-        """Form must have 4 fields"""
+        """Form must have 4 fields."""
         form = SubscriptionForm()
         expected = ['name', 'cpf', 'email', 'phone']
         self.assertSequenceEqual(expected, list(form.fields))
@@ -23,6 +23,21 @@ class SubscriptionFormTest(TestCase):
         """Name must be capitalized."""
         form = self.make_validated_form(name='HENRIQUE bastos')
         self.assertEqual('Henrique Bastos', form.cleaned_data['name'])
+
+    def test_email_is_optional(self):
+        """Email is optional."""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Phone is optional."""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and Phone are optional, but one must be informed."""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
 
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
